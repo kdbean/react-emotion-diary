441b8e0aedcc3fe83c3d99b8984ccf39f4330676
kdbean <kdbean.dev@gmail.com>
2025-04-15 23:59:05 +0900

    chore: extract emotionList to constants/emotions.js and apply import

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,20 +4,7 @@ import Button from "./Button";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-/**
- * @constant emotionList
- * Predefined list of emotions to be selected by user.
- * Each object includes:
- * - emotionId: numerical identifier
- * - emotionName: label displayed under the emotion icon
- */
-const emotionList = [
-  { emotionId: 1, emotionName: "Perfect" },
-  { emotionId: 2, emotionName: "Good" },
-  { emotionId: 3, emotionName: "Okay" },
-  { emotionId: 4, emotionName: "Bad" },
-  { emotionId: 5, emotionName: "Terrible" },
-];
+import { emotionList } from "../constants/emotions";
 
 /**
  * @function getStringedDate
---
+++ src/constants/emotions.js
@@ -0,0 +1,14 @@
+/**
+ * @constant emotionList
+ * Predefined list of emotions to be selected by user.
+ * Each object includes:
+ * - emotionId: numerical identifier
+ * - emotionName: label displayed under the emotion icon
+ */
+export const emotionList = [
+  { emotionId: 1, emotionName: "Perfect" },
+  { emotionId: 2, emotionName: "Good" },
+  { emotionId: 3, emotionName: "Okay" },
+  { emotionId: 4, emotionName: "Bad" },
+  { emotionId: 5, emotionName: "Terrible" },
+];
